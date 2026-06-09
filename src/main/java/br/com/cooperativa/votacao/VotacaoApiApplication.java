1cd71ec796733d949bd11916eb41e5ae9d707254
gabrielfogliatto <gabrielfogliatto@gmail.com>
2026-06-09 14:50:57 -0300

    Initial Spring Boot project

---
+++ src/main/java/br/com/cooperativa/votacao/VotacaoApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.cooperativa.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotacaoApiApplication.class, args);
+    }
+}
